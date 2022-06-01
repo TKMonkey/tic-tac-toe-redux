e973041fddd7a4a22aab647dc6d44f8bd6a5d55b
Juan E Quintero R <juequinterore666@gmail.com>
2022-06-01 17:00:10 -0500

    feat: add play movement use case

---
+++ src/domain/use_cases/game/play_movement.ts
@@ -0,0 +1,15 @@
+import { Game } from "../../entities/game";
+import { Movement } from "../../value_objects";
+import { CommandUseCase } from "../command_use_case";
+
+export interface PlayMovementParam {
+  readonly game: Game;
+  readonly movement: Movement;
+}
+
+export class PlayMovement implements CommandUseCase<PlayMovementParam> {
+  execute(input: PlayMovementParam): void {
+    const { game, movement } = input;
+    game.playMovement(movement);
+  }
+}
