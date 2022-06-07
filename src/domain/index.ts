9de4cc2bddfacab3a9efb22af4585072453ce627
Juan E Quintero R <juequinterore666@gmail.com>
2022-06-07 16:14:17 -0500

    export repositories

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,3 +1,4 @@
 export * from "./entities/";
+export * from "./repositories";
 export * from "./use_cases";
 export * from "./value_objects/";
---
+++ src/domain/repositories/index.ts
@@ -0,0 +1,1 @@
+export * from "./i_tic_tac_toe_repository";
