f84a4d9d50d16d4716952260698694e5750b67c1
Juan E Quintero R <juequinterore666@gmail.com>
2022-06-07 16:15:32 -0500

    feat: extract controller creation logic to prevent multiple creations.

--- src/view/hooks/use_tic_tac_toe_controller.ts
+++ src/view/hooks/use_tic_tac_toe_controller.ts
@@ -1,22 +1,58 @@
 import { useRef } from "react";
-import { Game, GetGameWithPreviousState, PlayMovement } from "../../domain";
-import { TicTacToeController, ITicTacToePresenter } from "../../presentation";
+import { KeyValueStorageLocalStorage } from "../../data_source/key_value_storage";
+import {
+  LocalDataSource,
+  RemoteDataSource,
+} from "../../data_source/tic_tac_toe";
+import {
+  // Game,
+  GetGameWithPreviousState,
+  GetInitialGame,
+  PlayMovement,
+  PlayMovementAndSaveDecorator,
+} from "../../domain";
+import { SaveGame } from "../../domain/use_cases/storage";
+import { TicTacToeRepository } from "../../infrastructure";
+import {
+  TicTacToeController,
+  ITicTacToePresenter,
+  ITicTacToeController,
+} from "../../presentation";
 
 export const useITicTacToeControllerImplementation = (
   presenter: ITicTacToePresenter
 ) => {
-  const game = new Game();
+  const controllerRef = useRef<ITicTacToeController | null>(null);
+
+  function initializeController(): ITicTacToeController {
+    if (controllerRef.current === null) {
+      controllerRef.current = _createController(presenter);
+    }
+
+    return controllerRef.current!;
+  }
+
+  return initializeController();
+};
+
+function _createController(presenter: ITicTacToePresenter) {
+  const keyValueStorage = new KeyValueStorageLocalStorage();
+  const localDataSource = new LocalDataSource(keyValueStorage);
+  const remoteDataSource = new RemoteDataSource();
+  const repository = new TicTacToeRepository(localDataSource, remoteDataSource);
+  const getInitialGame = new GetInitialGame(repository);
   const playMovementUseCase = new PlayMovement();
+  const saveGame = new SaveGame(repository);
+  const playMovementAndSaveDecorator = new PlayMovementAndSaveDecorator(
+    saveGame,
+    playMovementUseCase
+  );
   const getGameWithPreviousState = new GetGameWithPreviousState();
 
-  const controllerRef = useRef(
-    new TicTacToeController(
-      game,
-      presenter,
-      playMovementUseCase,
-      getGameWithPreviousState
-    )
+  return new TicTacToeController(
+    presenter,
+    getInitialGame,
+    playMovementAndSaveDecorator,
+    getGameWithPreviousState
   );
-
-  return controllerRef.current;
-};
+}
